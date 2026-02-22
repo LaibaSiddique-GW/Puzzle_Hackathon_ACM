439697c743f9bddf15dd8a0f25e77a8fab2749f1
Sam-Haendell W. Thosiac <149746410+samthosiac@users.noreply.github.com>
2026-02-21 20:39:32 -0500

    Changes related to our design and our settings.

---
+++ client/js/sound.js
@@ -0,0 +1,23 @@
+const soloSound = new Audio('solo_sound.mp3');
+const duoSound = new Audio('duo_sound.mp3');
+
+soloSound.volume = 0.25;
+duoSound.volume = 0.25;
+
+function playSoloSound() {
+  soloSound.currentTime = 0;
+  soloSound.play();
+  setTimeout(() => {
+    soloSound.pause();
+    soloSound.currentTime = 0;
+  }, 1000);
+}
+
+function playDuoSound() {
+  duoSound.currentTime = 0;
+  duoSound.play();
+  setTimeout(() => {
+    duoSound.pause();
+    duoSound.currentTime = 0;
+  }, 1000);
+}
